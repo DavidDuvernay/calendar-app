b94c06de72e9273c58d9b062438e0880cbb214c7
David Duvernay <david.anderson0612@gmail.com>
2021-02-13 00:33:06 -0500

    Funtionality 100%
    - .env for development and production.
    - auth.js with corresponding actions.
    - LoginScreen now works with state.
    - Navbar propertly shows user's name and logut button working.
    - using fetch to communicate with endpoints.
    - useForm to manage login/register form.
    - authReducer recibing all auth actions and types petitions.
    - Added Public/Private routes management.

---
+++ src/actions/auth.js
@@ -0,0 +1,89 @@
+import Swal from "sweetalert2";
+import { fetchWithoutToken, fetchWithToken } from "../helpers/fetch";
+import { types } from "../types/types";
+
+
+export const startLogin = (email, password) => {
+
+    return async(dispatch) => {
+        
+        const resp = await fetchWithoutToken('auth', { email, password}, 'POST');
+        const body = await resp.json()
+        
+        if(body.ok){
+            localStorage.setItem('token', body.token)
+            localStorage.setItem('token-init-date', new Date().getTime())
+
+            dispatch(login({
+                uid: body.uid,
+                name: body.name
+            }))
+        } else {
+
+            Swal.fire('Error', 'Wrong email or password', 'error')
+        }
+    }
+};
+
+export const startRegister = (email, password, name) => {
+
+    return async(dispatch)=> {
+        
+        const resp = await fetchWithoutToken('auth/new', {email, password, name}, 'POST');
+        const body = await resp.json()
+
+        if(body.ok){
+
+            localStorage.setItem('token', body.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+
+            dispatch(login({
+                uid: body.uid,
+                name: body.name
+            }))
+
+        } else {
+            
+            return Swal.fire('Error', 'Failed to create user', 'error')
+        }
+    }
+};
+
+export const startChecking = () => {
+
+    return async(dispatch) => {
+        const resp = await fetchWithToken('auth/renew');
+        const body = await resp.json()
+
+        if(body.ok){
+            localStorage.setItem('token', body.token)
+            localStorage.setItem('token-init-date', new Date().getTime())
+
+            dispatch(login({
+                uid: body.uid,
+                name: body.name
+            }))
+
+        } else {
+            dispatch(checkingFinish)
+        }
+    }  
+};
+
+export const startLogout = () => {
+        
+    return (dispatch) => {
+
+        localStorage.clear()
+        dispatch(logout())
+    }
+}
+
+const login = (user) => ({
+    type: types.authLogin,
+    payload: user
+});
+
+const checkingFinish = () => ({type: types.authCheckingFinish});
+
+const logout = () => ({type: types.authLogout})
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,44 @@
+import { types } from "../types/types";
+
+const initialState = {
+    checking: true,
+    // uid: null,
+    // name: null
+}
+
+export const authReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        
+        case types.authLogin:
+
+            return {
+                ...state,
+                checking: false,
+                ...action.payload
+            }
+
+        case types.authStartRegister:
+            
+            return {
+                ...state,
+                checking: false
+            }
+
+        case types.authCheckingFinish:
+
+            return {
+                ...state,
+                checking: false
+            }
+
+        case types.authLogout:
+            
+            return {
+                checking: false
+            }
+
+        default:
+            return state;
+    }
+};
